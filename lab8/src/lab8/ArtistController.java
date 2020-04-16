5a881e428adcf79bb9afd52ea42eb535f025923a
Claudiu Timofte <59256981+cltty@users.noreply.github.com>
2020-04-16 10:41:43 +0300

    Update ArtistController.java

--- lab8/src/lab8/ArtistController.java
+++ lab8/src/lab8/ArtistController.java
@@ -5,33 +5,67 @@
  */
 package lab8;
 
+import java.sql.*;
 /**
  *
  * @author Claudiu
  */
 public class ArtistController {
-    List<Array> artists;
+    private Connection connection;
     
-    public ArtistController() throws SQLException {
-        Database db = new Database();
-        Connection dbConn = dbConn.createConnection("myDB", "");
-        Statement stmt = dbConn.createStatement();
-        ResultSet rows = stmt.executeQuery("SELECT * FROM artists");
+    public ArtistController(Database databaseInstance){
+        this.connection = databaseInstance.connection;
+    }
+    
+    public void addArtist(Artist artistToAdd) {
+        String sqlFindMaxID = "SELECT max(id) FROM artisti";
+        int maxID = -1;
         
-        while (true == rows.next()) {
-            Artist artist = new Artist(rows.getInt(1), rows.getString(2), rows.getString(3));
-            artists.add(artist);
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlFindMaxID);
+
+            if (true == rs.next()) {
+                maxID = rs.getInt(1);
+                System.out.println("ID MAXIM : " + maxID);
+            }
+            maxID ++;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        String sql = "INSERT INTO artisti VALUES(?, ?, ?)";
+        try {
+            PreparedStatement prepStmt = connection.prepareStatement(sql);
+            prepStmt.setInt(1, maxID);
+            prepStmt.setString(2, artistToAdd.getNume());
+            prepStmt.setString(3, artistToAdd.getCountry());
+            
+            
+            prepStmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
     
-    public void create(String name, String country) throws SQLException {
-        Database db = new Database();
-        Connection dbConn = dbConn.createConnection("myDB", "");
-        Statement stmt = dbConn.createStatement();
-        Artist artist = new Artist(artists.size() + 1, name, country);
-        artists.add(artist);
-        stmt.executeUpdate("INSERT INTO artists values(" + (artists.size() + 1) + ", " + name + ", " + country + ")");
-    }
-    
-    
+    public Artist findArtistByName(String name) {
+        String sql = "SELECT * FROM artisti WHERE NAME = ?";
+        Artist artist = null;
+        
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, name);     
+            ResultSet rs = stmt.executeQuery();
+            
+            if (true == rs.next()) {
+                artist = new Artist(rs.getInt(1), rs.getString(2), rs.getString(3));
+            }
+           
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        
+        return artist;
+    }    
 }
